cdc683cdfafabe402ab591b889fe152bd060c46a
Shantanu Kapse <shantanu.sk05@gmail.com>
2023-07-14 17:25:52 +0530

    add mongoose connectivity

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static("public"));
-var item = ["Study","Music","Eat"];
+// var item = ["Study","Music","Eat"];
+
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+const itemSchema =new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("items",itemSchema);
+
+
+
+
+
 app.get("/", function(req, res){
   var d = new Date();
   var opn ={
@@ -15,13 +27,42 @@ app.get("/", function(req, res){
     month: "long"
   };
   d = d.toLocaleDateString("en-US",opn);
-  res.render("list",{mark: d, itm: item});
+
+  Item.find()
+  .then((items)=>{
+    if(items.length==0){
+      //default
+      const task1 = new Item({
+        name:"task1"
+      })
+      const task2 = new Item({
+        name:"task2"
+      })
+      const task3 = new Item({
+        name:"task3"
+      })
+      Item.insertMany([task1,task2,task3]);
+      res.redirect("/");
+    }
+
+    res.render("list",{mark: d, itm: items});
+  })
+
 });
 
 app.post("/",(req,res)=>{
   console.log("add");
-  item.push(req.body.task);
+  const itemName = req.body.task;
+  const ad = new Item({name:itemName});
+  ad.save();
+  // item.push(req.body.task);
   res.redirect("/");
+})
+
+app.post("/delete",(req,res)=>{
+  Item.findOneAndDelete({_id: req.body.check})
+  .then(res.redirect("/"));
+
 })
 
 app.post("/reset",(req,res)=>{
